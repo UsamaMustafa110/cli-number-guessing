69b71595e39fe12ab8ec1e7a812eb794359364e8
USAMA MUSTAFA <hu95130@gmail.com>
2024-03-24 01:43:08 +0500

    simple cli number guessing game

---
+++ index.ts
@@ -0,0 +1,18 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+
+const answer = await inquirer.prompt([
+  {
+    message: "enter your first guess",
+    type: "number,",
+    name: "UserGuessedNumber",
+  },
+]);
+if (answer.UserGuessedNumber === randomNumber) {
+  console.log("Congratulations! you guessed it right");
+} else {
+  console.log("Ah! your guess is not right, try again");
+}
